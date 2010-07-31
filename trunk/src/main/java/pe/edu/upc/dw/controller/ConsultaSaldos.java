38a57bf786abce6af77faa993fb573869d9ef152
oalvarez86 <oalvarez86@cfdd6e8c-9981-bb7a-b41d-55e3d3f87501>
2010-07-31 16:52:33 +0000

    Avances en capa de presentación CJulcapoma

---
+++ trunk/src/main/java/pe/edu/upc/dw/controller/ConsultaSaldos.java
@@ -0,0 +1,81 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.edu.upc.dw.controller;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import pe.edu.upc.dw.model.Alquiler;
+import pe.edu.upc.dw.model.Cliente;
+import pe.edu.upc.dw.model.ConsultaMovimiento;
+import pe.edu.upc.dw.model.Local;
+import pe.edu.upc.dw.model.Tarjeta;
+
+/**
+ *
+ * @author Carlos
+ */
+public class ConsultaSaldos extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        ConsultaMovimiento conmov= new ConsultaMovimiento();
+        Local local=new Local(1, "San Isidro", "Av. Canaval y Moreyra 354", "234232", "50");
+        Date now = new Date();
+        DateFormat df = DateFormat.getDateInstance();
+        String s = df.format(now).toUpperCase();
+
+        String linktype=req.getParameter("linktype");
+
+        req.setAttribute("fechaactual", s);
+
+        HttpSession session = req.getSession();
+        Cliente cliente  = (Cliente)session.getAttribute("usuario");
+        
+        req.setAttribute("tipovisor", "C");
+
+        req.setAttribute("cliente", cliente);
+
+        if ( linktype.equals("saldo")  ){
+            linktype = "card_account.jsp";
+        }
+        if ( linktype.equals("movimiento")){
+            Tarjeta tarjeta =new Tarjeta();
+            tarjeta =cliente.getTarjeta();
+            linktype = "card_mov.jsp";
+            conmov.getAlquileres().add(new Alquiler(1,"12/02/2010","12:49","13:49",12.00,"XBO2323",tarjeta,local,null));
+            conmov.getAlquileres().add(new Alquiler(2,"15/03/2010","12:49","13:49",12.00,"FJU7483",tarjeta,local,null));
+            conmov.getAlquileres().add(new Alquiler(3,"21/03/2010","12:49","13:49",12.00,"AO20394",tarjeta,local,null));
+            conmov.getAlquileres().add(new Alquiler(4,"27/04/2010","12:49","13:49",10.00,"AO20394",tarjeta,local,null));
+            conmov.getAlquileres().add(new Alquiler(5,"12/05/2010","12:49","13:49",12.00,"QPO3983",tarjeta,local,null));
+            conmov.setFechadesde(req.getParameter("fechadesde"));
+            conmov.setFechahasta(req.getParameter("fechahasta"));
+            if (conmov.getFechadesde() == null){
+                conmov.setFechadesde(s);
+                conmov.setFechahasta(s);
+            }
+            req.setAttribute("conmov", conmov);            
+            req.setAttribute("cliente", cliente);
+        }
+        req.getRequestDispatcher(linktype).forward(req, resp);
+
+    }
+
+    
+   
+    
+}
---
+++ trunk/src/main/webapp/header_isl.jsp
@@ -0,0 +1,10 @@
+<%@page contentType="text/html" pageEncoding="windows-1252"%>
+    <div id="menu">
+	<ul>
+		<li class="active"><a href="index.jsp" accesskey="1" title="">Inicio</a></li>
+	</ul>
+</div>
+<!-- end #menu -->
+<div id="header">
+	<h1></h1>
+</div>
